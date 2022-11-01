f9a7abad09e00ef0a0974f65084bb064b8a86fc9
vn54osf <Samuel.Garrido@walmart.com>
2022-10-31 19:52:04 -0600

    Initial Commit

---
+++ Shared/GlobedexApp.swift
@@ -0,0 +1,20 @@
+//
+//  GlobedexApp.swift
+//  Shared
+//
+//  Created by Samuel Arturo Garrido Sánchez on 2022-10-31.
+//
+
+import SwiftUI
+
+@main
+struct GlobedexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
